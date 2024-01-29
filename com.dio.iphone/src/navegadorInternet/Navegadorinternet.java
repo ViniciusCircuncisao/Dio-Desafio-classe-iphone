a288c58c3d0f97505fe304ef63e324662881b005
ViniciusCircuncisao <viniciusfelipe2015.vc@gmail.com>
2024-01-29 16:37:07 -0300

    Dio - Desafio Iphone

---
+++ com.dio.iphone/src/navegadorInternet/Navegadorinternet.java
@@ -0,0 +1,7 @@
+package navegadorInternet;
+
+public interface Navegadorinternet {
+    public void exibirPagina();
+    public void adicionarNovaAba();
+    public void atualizarPagina();
+}
---
+++ com.dio.iphone/src/usuario.java
@@ -0,0 +1,9 @@
+public class usuario {
+    public static void main(String[] args) {
+        Iphone celular = new Iphone(); 
+        celular.ligar(); 
+        celular.selecionarMusica(); 
+        celular.exibirPagina();
+    }
+    
+}
